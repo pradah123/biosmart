3ebe550ac09efeab3845ab97a6851c06ddbc2dae
Priyank SSD <priyankgt@gmail.com>
2021-12-09 10:46:49 +0530

    Enqueue jobs in SQS

--- lib/biosmart.rb
+++ lib/biosmart.rb
@@ -1,11 +1,27 @@
 module BioSmart
+
     def self.enqueue_sightings_update(for_region_id:)
-        DownloadableRegion.where(region_id: for_region_id).find_each do |dr|
+        region = ENV["SQS_REGION"] || "ap-southeast-2"
+        queue_url = ENV["SQS_URL"] || "https://sqs.ap-southeast-2.amazonaws.com/461130176523/biosmart-import-queue-test"
+        sqs_client = Aws::SQS::Client.new(region: region)
+
+        DownloadableRegion.where(
+            region_id: for_region_id, 
+            deleted_at: nil
+        ).find_each do |dr|
             event_json = {
                 "app-id" => dr.app_id,
                 params: dr.params
             }.to_json
-            puts 'curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d ' + "'#{event_json}'"
+            begin
+                sqs_client.send_message(
+                    queue_url: queue_url,
+                    message_body: event_json
+                )
+            rescue StandardError => e
+                puts "Error sending message: #{e.message}"
+            end
         end
-    end
+    end    
+    
 end
