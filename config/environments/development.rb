548008e634fd86957ffae490c71b3b6a0d536f37
Priyank SSD <priyankgt@gmail.com>
2021-12-10 17:47:56 +0530

    Log activerecord queries

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,7 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
+    config.active_record.logger = Logger.new(STDOUT)
 
 
   # Raises error for missing translations.
