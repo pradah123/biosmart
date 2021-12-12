168c80ac3c7b6813e29e1863c043cb222ef20995
Priyank SSD <priyankgt@gmail.com>
2021-12-12 21:21:24 +0530

    Setup cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'biosmart.life'
-        resource '/api/count/*',
-            headers: :any,
-            methods: :get,
-            if: proc { |env| env['HTTP_HOST'] =~ 'biosmart.life' }
-        resource '/api/observations',
-            headers: :any,
-            methods: :get,
-            if: proc { |env| env['HTTP_HOST'] =~ 'biosmart.life' }
+        origins '*'
+        resource '*', 
+            headers: ['DNT','X-CustomHeader','Keep-Alive','User-Agent','X-Requested-With','If-Modified-Since','Cache-Control','Content-Type'], 
+            methods: [:get, :options]
     end
 end
