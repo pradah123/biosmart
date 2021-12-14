f233fb9e6c5ce12d9a924009ad99c26618bef783
Priyank SSD <priyankgt@gmail.com>
2021-12-14 18:50:19 +0530

    Enqueue dates in download tasks using contest data

--- lib/biosmart.rb
+++ lib/biosmart.rb
@@ -1,25 +1,32 @@
 module BioSmart
 
-    def self.enqueue_sightings_update(for_region_id:)
+    def self.enqueue_sightings_update(for_contest_id:)
         region = ENV["SQS_REGION"] || "ap-southeast-2"
         queue_url = ENV["SQS_URL"] || "https://sqs.ap-southeast-2.amazonaws.com/461130176523/biosmart-import-queue-test"
         sqs_client = Aws::SQS::Client.new(region: region)
 
-        DownloadableRegion.where(
-            region_id: for_region_id, 
+        RegionContest.where(
+            contest_id: for_contest_id, 
             deleted_at: nil
-        ).find_each do |dr|
-            event_json = {
-                "app-id" => dr.app_id,
-                params: dr.params
-            }.to_json
-            begin
-                sqs_client.send_message(
-                    queue_url: queue_url,
-                    message_body: event_json
-                )
-            rescue StandardError => e
-                puts "Error sending message: #{e.message}"
+        ).includes(:contest).each do |rc|
+            DownloadableRegion.where(
+                region_id: rc.region_id,
+                deleted_at: nil
+            ).find_each do |dr|
+                dr.params["d1"] = rc.contest.begin_at.strftime('%Y-%m-%d')
+                dr.params["d2"] = rc.contest.end_at.strftime('%Y-%m-%d')
+                event_json = {
+                    "app-id" => dr.app_id,
+                    params: dr.params
+                }.to_json
+                begin
+                    sqs_client.send_message(
+                        queue_url: queue_url,
+                        message_body: event_json
+                    )
+                rescue StandardError => e
+                    puts "Error sending message: #{e.message}"
+                end
             end
         end
     end    
--- lib/tasks/biosmart.rake
+++ lib/tasks/biosmart.rake
@@ -1,19 +1,15 @@
 require "biosmart.rb"
 
 namespace :biosmart do
-
     namespace :sightings do
-
         desc "Update sightings"
-        task :update, [:region_id] => :environment do |t, args|
-            region_id = args[:region_id]
-            if region_id.blank?
-                logger.error "Please provide a valid region ID"
+        task :update, [:contest_id] => :environment do |t, args|
+            contest_id = args[:contest_id]
+            if contest_id.blank?
+                logger.error "Please provide a valid contest ID"
                 exit
             end
-			BioSmart.enqueue_sightings_update(for_region_id: region_id)
+			BioSmart.enqueue_sightings_update(for_contest_id: contest_id)
         end
-
     end
-
 end
