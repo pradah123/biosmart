d564b7dde6d54c2609cb0b05744f31944ae53bd9
Peter Williams <prw20042004@yahoo.co.uk>
2022-04-13 07:02:08 +0000

    Peter comments

--- app/jobs/observations_fetch_job.rb
+++ app/jobs/observations_fetch_job.rb
@@ -8,10 +8,13 @@ class ObservationsFetchJob < ApplicationJob
       contest.participations.in_competition do |participant|
         participant.data_sources.each do |data_source|
           # Skip download if no subregion exists
-          # PRW: we don't need this if statement- its an extra dbquery we don't need
+          # Peter: we don't need this if statement- its an extra dbquery we don't need
           # you have the same where statement inside the function which will return an empty array
           # if there are no subregions. So we can just remove the if statement here.
           if Subregion.exists?(region_id: participant.region_id, data_source_id: data_source.id)
+            # Peter: the times here should not be the complete time range of the contest- it should 
+            # be "time of last fetch" and "time of next fetch", otherwise we will waste a lot of time 
+            # processing data which has not changed.
             data_source.fetch_observations participant.region, contest.starts_at, contest.ends_at
           end
         end
