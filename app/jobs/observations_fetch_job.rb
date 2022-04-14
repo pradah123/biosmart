81daced5aac4bee1099750943350615f6d239c07
Priyank SSD <priyankgt@gmail.com>
2022-04-14 18:29:32 +0530

    Fix observations_fetch_job.rb

--- app/jobs/observations_fetch_job.rb
+++ app/jobs/observations_fetch_job.rb
@@ -5,13 +5,16 @@ class ObservationsFetchJob < ApplicationJob
     Delayed::Worker.logger.info "\n\n\n\n>>>>>>>>>> fetching observations"
     
     Contest.in_progress.each do |contest|
-      contest.participations.in_competition do |participant|
+      contest.participations.in_competition.each do |participant|
         participant.data_sources.each do |data_source|
-          observations = data_source.fetch_observations participant.region
-          ObservationsCreateJob.perform_later data_source, observations
+          # Peter: the times here should not be the complete time range of the contest- it should 
+          # be "time of last fetch" and "time of next fetch", otherwise we will waste a lot of time 
+          # processing data which has not changed.
+          data_source.fetch_observations participant.region, contest.starts_at, contest.ends_at
         end
       end
-    end      
+    end
+
 
     Delayed::Worker.logger.info ">>>>>>>>>> completed\n\n\n\n"
   end
