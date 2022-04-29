712b2229a9852ad0ccd9bc1fe7bc7368e36b765f
Peter Robert Williams <prw20042004@yahoo.co.uk>
2022-04-29 20:21:22 +0900

    reset contest times

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -16,7 +16,7 @@ class Participation < ApplicationRecord
     # contest model start and end datetimes are not utc- they refer to the time in the local time of each region. 
     # the actual start and end are those datetimes in the timezone of the region, in utc.
     #
-    offset = region.timezone_offset_mins.abs.minutes
+    offset = region.timezone_offset_mins.nil? ? 0 : region.timezone_offset_mins.abs.minutes
     if offset<0
       update_column :starts_at, (contest.starts_at + offset)
       update_column :ends_at, (contest.ends_at + offset)
