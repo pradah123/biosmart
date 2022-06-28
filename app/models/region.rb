f33affbb32450e6f386db05786d9a506a11bedee
Priyank SSD <priyankgt@gmail.com>
2022-06-28 18:50:27 +0530

    Trello 307: QGame data not appearing

--- app/models/region.rb
+++ app/models/region.rb
@@ -205,12 +205,11 @@ class Region < ApplicationRecord
 
   def self.get_multipolygon_from_raw_polygon_json raw_polygon_json
     polygon_geojson = JSON.parse raw_polygon_json
-    #polygon_geojson = [polygon_geojson] unless polygon_geojson.kind_of?(Array)
-    return 'MULTIPOLYGON(())' if polygon_geojson['coordinates'].nil?
-
+    polygon_geojson = [polygon_geojson] unless polygon_geojson.kind_of?(Array)
+    
+    polygon_strings = []
     polygon_geojson.each do |rpj|
       rpj['coordinates'].push rpj['coordinates'][0] unless rpj['coordinates'].empty?
-      coordinates = rpj['coordinates'].map { |c| "#{c[1]} #{c[0]}" }.join ', ' # check order
       coordinates = rpj['coordinates'].map { |c| "#{c[0]} #{c[1]}" }.join ','
       polygon_strings.push "((#{ coordinates }))"
     end
