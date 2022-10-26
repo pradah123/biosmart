b1151e2e797377262434c5ab2dc5b5ed193e8b73
Priyank <priyankgt@gmail.com>
2022-10-26 11:39:58 +0530

    Add comments & links for code understanding

--- services/application.rb
+++ services/application.rb
@@ -4,12 +4,19 @@ require 'dry/matcher/result_matcher'
 require 'dry/monads'
 require 'dry/monads/do'
 
+# Refer following to understand the working of this class
+# https://veerpalbrar.github.io/blog/2021/11/26/Include,-Extend,-and-Prepend-In-Ruby
+# https://juzer-shakir.medium.com/accessing-module-in-root-in-ruby-8eb46dbb38e1
+
 module Service
   module Application
     module ClassMethods
       def call(params, &block)
+        # Executed InstanceMethods execute method since it is prepended
         service_outcome = new.execute(params)
         if block_given?
+          # Refer https://github.com/dry-rb/dry-matcher/blob/main/lib/dry/matcher/result_matcher.rb
+          # to understand how ResultMatcher works
           Dry::Matcher::ResultMatcher.call(service_outcome, &block)
         else
           service_outcome
@@ -21,6 +28,8 @@ module Service
       include Dry::Monads[:result, :do]
 
       def execute(params)
+        # Refer https://dry-rb.org/gems/dry-monads/1.3/do-notation/
+        # to understand working of the following line
         yield validate_params(params)
         super(params)
       end
@@ -44,8 +53,15 @@ module Service
       end
     end
 
+    # Whenever a class includes a module, 
+    # it runs the self.included callback on the module
     def self.included(klass)
+      # With prepend, the module is added before the class in the ancestor chain. 
+      # This means ruby will look at the module to see if an instance method is 
+      # defined before checking if it is defined in the class.
       klass.prepend InstanceMethods
+      # When a class extend’s a module, it adds the module methods as class methods 
+      # on the class.
       klass.extend ClassMethods
     end
   end
