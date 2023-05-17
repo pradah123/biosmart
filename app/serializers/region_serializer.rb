09a6dab835675ef6624c192026dd35be93973990
pradah <prajakta@earthguardians.life>
2023-05-17 09:34:14 +0000

    Trello 542: Return slug in regions APIs

--- app/serializers/region_serializer.rb
+++ app/serializers/region_serializer.rb
@@ -2,7 +2,7 @@ class RegionSerializer
   include JSONAPI::Serializer
   attributes :id, :name, :description, :logo_image_url, :logo_image, :header_image_url,
              :header_image, :raw_polygon_json, :region_url, :bioscore, :lat, :lng,
-             :lat_input, :lng_input, :polygon_side_length, :status,
+             :lat_input, :lng_input, :polygon_side_length, :status, :slug,
              :observations_count, :species_count, :people_count, :identifications_count,
              :physical_health_score, :mental_health_score,
              :subscription, :display_flag
