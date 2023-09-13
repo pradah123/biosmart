674e83c5299a0f11b2a46f941c614ef359d76066
pradah <prajakta@earthguardians.life>
2023-09-13 10:34:44 +0000

    Trello 565: Minor thumbnail resize related changes

--- lib/common/file_operations.rb
+++ lib/common/file_operations.rb
@@ -20,8 +20,13 @@ module FileOperations
     file_path = "#{Rails.root}/public/#{thumbnail_file_name}.png"
     begin
       image = MiniMagick::Image.open(original_image_path)
-      image.resize "100 X 100"
-      image.write(file_path)
+      if image.width > 300 || image.height > 300
+        image.resize "300 X 300"
+        image.write(file_path)
+      else
+        actual_img = IO.binread(original_image_path)
+        IO.binwrite(file_path, actual_img)
+      end
       return 1
     rescue => e
       Rails.logger.error "Unable to create logo thumbnail image #{file_path}"
